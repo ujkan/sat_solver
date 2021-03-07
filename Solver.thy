dbda7370e3476e751673782485f9511e5fe9b588
Ujkan Sulejmani <ujkan.sulejmani@tum.de>
2021-03-07 20:10:40 +0100

    added basic formula definition & eval function

--- Solver.thy
+++ Solver.thy
@@ -2,6 +2,18 @@ theory Solver
   imports Main
 begin
 
+datatype Formula = Var string | Const bool | Not Formula | And Formula Formula | Or Formula Formula
+
+type_synonym state = "string \<Rightarrow> bool"
+
+fun eval :: "Formula \<Rightarrow> state \<Rightarrow> bool"
+  where
+    "eval (Const b) s = b"
+  | "eval (Var x) s = s x"
+  | "eval (Not F) s = (\<not>(eval F s))"
+  | "eval (And F G) s = (eval F s \<and> eval G s)"
+  | "eval (Or F G) s = (eval F s \<or> eval G s)"
+
 
 
 
