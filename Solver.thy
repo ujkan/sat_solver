15fecff211d67c3d4a86142b2544559ef5ef45ce
Ujkan Sulejmani <ujkan.sulejmani@tum.de>
2021-03-07 20:23:40 +0100

    added cnf definition and basic eval function(s)

--- Solver.thy
+++ Solver.thy
@@ -2,11 +2,11 @@ theory Solver
   imports Main
 begin
 
-datatype Formula = Var string | Const bool | Not Formula | And Formula Formula | Or Formula Formula
+datatype formula = Var string | Const bool | Not formula | And formula formula | Or formula formula
 
 type_synonym state = "string \<Rightarrow> bool"
 
-fun eval :: "Formula \<Rightarrow> state \<Rightarrow> bool"
+fun eval :: "formula \<Rightarrow> state \<Rightarrow> bool"
   where
     "eval (Const b) s = b"
   | "eval (Var x) s = s x"
@@ -14,9 +14,42 @@ fun eval :: "Formula \<Rightarrow> state \<Rightarrow> bool"
   | "eval (And F G) s = (eval F s \<and> eval G s)"
   | "eval (Or F G) s = (eval F s \<or> eval G s)"
 
+definition satisfiable :: "formula \<Rightarrow> bool"
+  where "satisfiable F \<equiv> (\<exists>s. eval F s = True)"
 
+lemma "satisfiable (Const True)"
+proof -
+  have "eval (Const True) (\<lambda>x. True)" by simp
+  thus ?thesis using satisfiable_def by simp
+qed
 
+lemma "\<not>satisfiable (Const False)"
+proof -
+  have "\<forall>s. eval (Const False) s = False" by simp
+  thus ?thesis using satisfiable_def by simp
+qed
 
+datatype literal = P string | N string
+
+type_synonym clauseCNF = "literal list"
+
+type_synonym formulaCNF = "clauseCNF list"
+
+fun leval :: "literal \<Rightarrow> state \<Rightarrow> bool"
+  where
+    "leval (P x) s = s x"
+  | "leval (N x) s = (\<not>s x)"
+
+fun ceval :: "clauseCNF \<Rightarrow> state \<Rightarrow> bool"
+  where
+    "ceval [] s = True"
+  | "ceval (l # ls) s = (leval l s \<or> ceval ls s)"
+
+fun evalCNF :: "formulaCNF \<Rightarrow> state \<Rightarrow> bool"
+  where
+    "evalCNF [] s = False"
+  | "evalCNF [c] s = ceval c s"
+  | "evalCNF (c # c' # cs) s = (ceval c s \<and> evalCNF (c' # cs) s)"
 
 
 end
